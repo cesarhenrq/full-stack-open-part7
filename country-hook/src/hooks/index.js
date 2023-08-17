2c0d1c086d9a574391467966614b30d634da9a5e
cesarhenrq <cesaarhvb@gmail.com>
2023-08-17 02:53:02 -0300

    7.7: country hook

---
+++ country-hook/src/hooks/index.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+
+import axios from "axios";
+
+export const useField = (type) => {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return {
+    type,
+    value,
+    onChange,
+  };
+};
+
+export const useCountry = (name) => {
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {
+    const fetchCountry = async () => {
+      try {
+        const response = await axios.get(
+          `https://studies.cs.helsinki.fi/restcountries/api/name/${name}`
+        );
+        setCountry({ data: response.data, found: true });
+      } catch (error) {
+        setCountry({ found: false });
+      }
+    };
+
+    if (name) {
+      fetchCountry();
+    }
+  }, [name]);
+
+  return country;
+};
